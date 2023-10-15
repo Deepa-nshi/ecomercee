4a8013b5e47e1801dbcb989d10b2c84baae749f7
Deepa-nshi <145038916+Deepa-nshi@users.noreply.github.com>
2023-10-15 14:07:39 +0530

    Add files via upload

---
+++ ecommerce.js
@@ -0,0 +1,9 @@
+const close=document.querySelector(".close");
+const open=document.querySelector(".ham");
+const menu=document.queryselector(".menu");
+close.addEventListener("click",()=>{
+    menu.style.visibility="hidden";
+})
+open.addEventListener("click",()=>{
+    menu.style.visibility="visible";
+})
